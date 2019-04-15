07db4bcd131fdc0bb4c8332a1831d5e20b0ade8e
buketbilgic <buketildam@gmail.com>
2019-04-15 21:02:46 +0300

    Initial commit

---
+++ Denizhan/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Denizhan
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_tikla = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_tikla
+            // 
+            this.btn_tikla.Location = new System.Drawing.Point(111, 56);
+            this.btn_tikla.Name = "btn_tikla";
+            this.btn_tikla.Size = new System.Drawing.Size(240, 23);
+            this.btn_tikla.TabIndex = 0;
+            this.btn_tikla.Text = "TIKLA";
+            this.btn_tikla.UseVisualStyleBackColor = true;
+            this.btn_tikla.Click += new System.EventHandler(this.btn_tikla_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(111, 108);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(476, 311);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btn_tikla);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_tikla;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ Denizhan/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SQLite;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Denizhan
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SQLiteConnection sqlite_conn;
+        private void btn_tikla_Click(object sender, EventArgs e)
+        {
+            SQLiteCommand sqlite_cmd;
+            using (sqlite_conn = new SQLiteConnection("Data Source=database.db;Version=3;New=True;Compress=True;"))
+            {
+                try
+                {
+                    sqlite_cmd = sqlite_conn.CreateCommand();
+                    sqlite_conn.Open();
+
+                    if (!TabloVarMi("tbl_Records", "Data Source=database.db;Version=3;New=True;Compress=True;"))
+                    {
+                        sqlite_cmd.CommandText = "CREATE TABLE tbl_Records (id INTEGER PRIMARY KEY AUTOINCREMENT, rc_date varchar(50));";
+                        sqlite_cmd.ExecuteNonQuery(); 
+                        MessageBox.Show("Tablo oluştu");
+                    }
+                    else
+                    {
+                        DataSet ds = new DataSet();
+                        sqlite_cmd.CommandText = "INSERT INTO tbl_Records (rc_date) VALUES ('" + DateTime.Now.ToLongTimeString() + "');";
+                        sqlite_cmd.ExecuteNonQuery();
+                        sqlite_cmd.CommandText = "SELECT rc_date FROM tbl_Records";
+                        var da = new SQLiteDataAdapter(sqlite_cmd.CommandText, sqlite_conn);
+                        da.Fill(ds);
+                        dataGridView1.DataSource = ds.Tables[0].DefaultView;
+                    }
+                }
+                catch
+                {
+                    MessageBox.Show("bağlantı hatası");
+                }
+            }
+        }
+        public bool TabloVarMi(String tableName, string sDatabaseName)
+        {
+            SQLiteDataReader sqlite_datareader;
+            SQLiteCommand sqlite_cmd = sqlite_conn.CreateCommand();
+            bool snc = false;
+            try
+            {
+                sqlite_cmd.CommandText = "SELECT name FROM sqlite_master WHERE type='table' AND name='" + tableName + "'";
+                sqlite_datareader = sqlite_cmd.ExecuteReader();
+                if (sqlite_datareader.Read())
+                {
+                    snc = true;
+                }
+                sqlite_datareader.Close();
+                return snc;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Tablo mevcut");
+                return false;
+            }
+        }
+    }
+}
